b8cd48bb6c94591bc65b9b668839919e6eb70ca3
Piero Hernandez <jeanpierohm@gmail.com>
2016-07-25 10:01:05 -0400

    Language correction

--- backend/appriesgo_backend/polls/apps.py
+++ backend/appriesgo_backend/polls/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
-    name = _('Polls')
+    name = _('Encuestas')
--- backend/appriesgo_backend/polls/models.py
+++ backend/appriesgo_backend/polls/models.py
@@ -5,77 +5,78 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Poll(models.Model):
-    name = models.CharField(max_length=200, verbose_name=_('Name'))
+    name = models.CharField(max_length=200, verbose_name=_('Nombre'))
 
     def __str__(self):
         return "%s" % self.name
 
     class Meta:
-        verbose_name = _('Poll')
-        verbose_name_plural = _('Polls')
+        verbose_name = _('Encuesta')
+        verbose_name_plural = _('Encuestas')
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, verbose_name=_('Name'))
+    name = models.CharField(max_length=50, verbose_name=_('Nombre'))
 
     def __str__(self):
         return "%s" % self.name
 
     class Meta:
-        verbose_name = _('Category')
-        verbose_name_plural = _('Categories')
+        verbose_name = _('Categoria')
+        verbose_name_plural = _('Categorias')
 
 
 class Question(models.Model):
-    text = models.CharField(max_length=300, verbose_name=_('Text'))
-    poll = models.ForeignKey(Poll, verbose_name=_('Poll'))
-    category = models.ForeignKey(Category, verbose_name=_('Category'))
+    text = models.CharField(max_length=300, verbose_name=_('Texto'))
+    poll = models.ForeignKey(Poll, verbose_name=_('Encuesta'))
+    category = models.ForeignKey(Category, verbose_name=_('Categoria'))
 
     def __str__(self):
         return "%s" % self.text
 
     class Meta:
-        verbose_name = _('Question')
-        verbose_name_plural = _('Questions')
+        verbose_name = _('Pregunta')
+        verbose_name_plural = _('Preguntas')
 
 
 class Choice(models.Model):
-    text = models.CharField(max_length=200, verbose_name=_('Text'))
-    value = models.IntegerField(verbose_name=_('Value'))
-    question = models.ForeignKey(Question, verbose_name=_('Question'))
+    text = models.CharField(max_length=200, verbose_name=_('Texto'))
+    value = models.IntegerField(verbose_name=_('Valor'))
+    question = models.ForeignKey(Question, verbose_name=_('Pregunta'))
 
     def __str__(self):
         return "%s :: %s" % (self.question.text, self.text)
 
     class Meta:
-        verbose_name = _('Choice')
-        verbose_name_plural = _('Choices')
+        verbose_name = _('Opcion')
+        verbose_name_plural = _('Opciones')
 
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, verbose_name=_('Question'))
-    choice = models.ForeignKey(Choice, verbose_name=_('Choice'))
-    user = models.ForeignKey(User, verbose_name=_('User'))
+    question = models.ForeignKey(Question, verbose_name=_('Pregunta'))
+    choice = models.ForeignKey(Choice, verbose_name=_('Opcion'))
+    user = models.ForeignKey(User, verbose_name=_('Usuario'))
     date_time = models.DateTimeField(default=datetime.datetime.now,
-                                     verbose_name=_('Date and time'))
+                                     verbose_name=_('Fecha y hora'))
 
     def __str__(self):
         return "%s : %s : %s" % (self.user.username, self.question.text,
                                  self.choice.text)
 
     class Meta:
-        verbose_name = _('Answer')
-        verbose_name_plural = _('Answers')
+        verbose_name = _('Respuesta')
+        verbose_name_plural = _('Respuestas')
 
 
 class Recommendation(models.Model):
-    text = models.TextField(verbose_name=_('Text'))
-    category = models.ForeignKey(Category, verbose_name=_('Category'))
-    score_lower_bound = models.IntegerField(verbose_name=_('Score lower bound'))
+    text = models.TextField(verbose_name=_('Texto'))
+    category = models.ForeignKey(Category, verbose_name=_('Categoria'))
+    score_upper_bound = models.IntegerField(verbose_name=
+                                            _('Cota supervior de puntaje'))
 
     def __str__(self):
         return "%s" % self.text
 
     class Meta:
-        verbose_name = _('Recommendation')
-        verbose_name_plural = _('Recommendations')
+        verbose_name = _('Recomendacion')
+        verbose_name_plural = _('Recomendaciones')
