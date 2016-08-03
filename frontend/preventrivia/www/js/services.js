cf68a4015a50d3b30c3fe64c7347d95f5080c536
Piero Hernandez <jeanpierohm@gmail.com>
2016-08-03 10:17:36 -0400

    Frontend: add Tip service

--- frontend/preventrivia/www/js/services.js
+++ frontend/preventrivia/www/js/services.js
@@ -3,7 +3,11 @@ angular.module('app.services', [])
 .factory('Question', ['ApiUrl', '$resource', function(ApiUrl, $resource){
   return $resource(ApiUrl + '/api/question/:id');
 }])
-
+    
+.factory('Tip', ['ApiUrl', '$resource', function(ApiUrl, $resource){
+  return $resource(ApiUrl + '/api/tip/:id');
+}])
+    
 .service('BlankService', [function(){
 
 }]);
