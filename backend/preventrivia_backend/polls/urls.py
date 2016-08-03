571a90fe077f6f9b93ef6b462cda4b6b4c4669c9
Piero Hernandez <jeanpierohm@gmail.com>
2016-08-03 10:19:38 -0400

    Frontend: add badge to show actual question number. Refactor the poll tab and add real tips to the Formation tab. Backend: add the Tip viewset to the API urls

--- backend/preventrivia_backend/polls/urls.py
+++ backend/preventrivia_backend/polls/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from .viewsets import AnswerViewSet, ChoiceViewSet, QuestionViewSet, \
-    RecommendationViewSet, CategoryViewSet, PollViewSet
-
+    RecommendationViewSet, CategoryViewSet, PollViewSet, TipViewSet
 
 # Router for RESTful API
 rest_router = DefaultRouter()
@@ -12,6 +11,7 @@ rest_router.register(r'question', QuestionViewSet)
 rest_router.register(r'recommendation', RecommendationViewSet)
 rest_router.register(r'category', CategoryViewSet)
 rest_router.register(r'poll', PollViewSet)
+rest_router.register(r'tip', TipViewSet)
 
 urlpatterns = [
     url(r'^', include(rest_router.urls)),
--- frontend/preventrivia/www/js/controllers.js
+++ frontend/preventrivia/www/js/controllers.js
@@ -1,27 +1,27 @@
 angular.module('app.controllers', [])
   
 .controller('pollCtrl', function($scope, Question) {
-  var questions = Question.query(function() {
-    for (var i=0; i<questions.length; i++) {
-      questions[i].index = i;
+  $scope.questions = Question.query(function() {
+    for (var i=0; i<$scope.questions.length; i++) {
+      $scope.questions[i].index = i;
     }
 
-    console.log(questions);
-    $scope.actual_question = questions[0];
+    console.log($scope.questions);
+    $scope.actual_question = $scope.questions[0];
   });
 
   $scope.showNextQuestion = function() {
-    if ($scope.actual_question.index == questions.length -1)
-      $scope.actual_question = questions[0];
+    if ($scope.actual_question.index == $scope.questions.length -1)
+      $scope.actual_question = $scope.questions[0];
     else
-      $scope.actual_question = questions[$scope.actual_question.index+1];
+      $scope.actual_question = $scope.questions[$scope.actual_question.index+1];
   };
 
   $scope.showPreviousQuestion = function() {
     if ($scope.actual_question.index == 0)
-      $scope.actual_question = questions[questions.length-1];
+      $scope.actual_question = $scope.questions[$scope.questions.length-1];
     else
-      $scope.actual_question = questions[$scope.actual_question.index-1];
+      $scope.actual_question = $scope.questions[$scope.actual_question.index-1];
   };
 })
    
@@ -29,7 +29,9 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('formationCtrl', function($scope) {
-
+.controller('formationCtrl', function($scope, Tip) {
+  $scope.tips = Tip.query(function() {
+    console.log($scope.tips);
+  });
 })
     
